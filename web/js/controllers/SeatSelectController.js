f70c0d2e16ea1aa4f28f620c7b3c4f05739b4d04
Oliver Steiner <oliver@mollo.ch>
2017-08-03 17:25:44 +0200

    new Class SeatSelectController

---
+++ web/js/controllers/SeatSelectController.js
@@ -0,0 +1,11 @@
+var SeatSelectController = (function () {
+    function SeatSelectController() {
+        console.log('seatSelectController');
+    }
+    SeatSelectController.prototype.start = function () {
+    };
+    SeatSelectController.prototype.addEventListeners = function () {
+        // toggle
+    };
+    return SeatSelectController;
+}());
