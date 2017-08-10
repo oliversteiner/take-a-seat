9f7bc370638ed492d109204a2f83c3c6bcdf0fa2
Oliver Steiner <oliver@mollo.ch>
2017-08-10 17:25:39 +0200

    Detail Auschnitt ok

--- src/ts/controllers/SeatSelectController.ts
+++ src/ts/controllers/SeatSelectController.ts
@@ -1,5 +1,7 @@
 class SeatSelectController {
 
+    public modus_maxiplan: boolean = false;
+    public modus_miniplan: boolean = false;
 
     constructor() {
         console.log('seatSelectController');
@@ -8,49 +10,98 @@ class SeatSelectController {
     }
 
     start() {
-        this.addEventListeners();
 
 
         sitzeVierschieben('links');
         sitzeVierschieben('rechts');
 
 
+        // Modus erkennen
+
+        // init
+        this.updateModus();
+        this.addEventListeners();
+
+        // after change
+
+
+        let rtime: any;
+        let timeout = false;
+        let delta = 200;
+        let scope = this;
+
+        $(window).resize(function () {
+            rtime = new Date();
+            if (timeout === false) {
+                timeout = true;
+                setTimeout(resizeend, delta);
+            }
+        });
+
+        function resizeend() {
+            let next_date: any = new Date();
+
+            if (next_date - rtime < delta) {
+                setTimeout(resizeend, delta);
+            } else {
+                timeout = false;
+                scope.updateModus();
+
+
+            }
+        }
+
+
     }
 
 
-    addEventListeners() {
+    updateModus = () => {
+
+        const scope = this;
+
+        if ($(window).width() > 500) {
+            // Desktop Screen
+
+            this.modus_maxiplan = false;
+            this.modus_miniplan = false;
+
+            $('#plan-well').removeClass('modus-maxiplan');
+        }
+        else {
+            // do something for small screens
+
+            this.modus_maxiplan = true;
+            this.modus_miniplan = false;
+
+            $('#plan-well').addClass('modus-maxiplan');
+            $('.abschnitt').addClass('modus-maxiplan-abschnitt-target');
+            $('.modus-maxiplan-abschnitt-target').click(function () {
+                scope.showDetails(this);
+            });
+
+        }
+
+        console.log('modus_maxiplan: ' + this.modus_maxiplan);
+        console.log('modus_miniplan: ' + this.modus_miniplan);
+
+    };
+
+    /**
+     *
+     *
+     *
+     */
+    addEventListeners = () => {
 
         // toggle
         let scope = this;
 
 
-        $('.abschnitt').click(function () {
-
-            if ($(window).width() < 500) {
-                // do something for small screens
-                scope.showDetails(this);
-            }
-        });
-
-
         $('.sitz').click(function () {
-
-
-            if ($(window).width() > 500) {
-                // do something for small screens
-
-                scope.waehleSitz(this);
-
-            }
-            else {
-
-                scope.showDetails(this);
-
-            }
-
-            // Wenn nur Abschnitt zu sehen ist
-
+            scope.waehleSitz(this);
         });
+
+
 
 
         $('table.preiskategorien tr').mouseover(this, function () {
@@ -71,76 +122,168 @@ class SeatSelectController {
 
     }
 
+    /**
+     *
+     * @param elem
+     */
     showDetails(elem: any) {
 
-        const $plan = $('#plan-content');
-        const $abschnitt_container = $('#abschnitt-container');
+
+        let scope = this;
+
+        // Anleitung ausblenden
+        $('#sitzplan-content .block-anleitung').hide();
+
+        // aufräumen
+        $('.ausschnitt-aktuell').removeClass('ausschnitt-aktuell');
+
+
+        const $well = $('#plan-well');
+        const $abschnitt_container = $('#plan-ausschnitt-container');
         const $elem = $(elem);
-        const class_uebersicht = 'uebersicht-modus';
+        const modus_miniplan = 'modus-miniplan';
 
         // übersichts Modus?
-        if( $plan.hasClass(class_uebersicht)){
+        if (this.modus_miniplan) {
 
-            $plan.removeClass(class_uebersicht);
+
+            $well.removeClass(modus_miniplan);
             $abschnitt_container.empty();
+
+
+            this.modus_miniplan = false;
+
 
         }
-        else{
+        else {
+
+            // Setze das System in den MIniplan:
+
+            this.modus_miniplan = true;
+
+
+
+            // Der Abschnitt darf
+
+
             $abschnitt_container.empty();
 
-            $plan.addClass(class_uebersicht);
+            $well.addClass(modus_miniplan);
 
             const $clone = $elem.clone(true);
 
+            $clone.find('.sitz').addClass('modus_miniplan');
+
+            // Den Click-Event entfernen
+            $clone.first().off();
+            $clone.removeClass('modus-maxiplan-abschnitt-target');
+
+
             $clone.appendTo($abschnitt_container);
 
-            $plan.addClass(class_uebersicht);
 
-            console.log(elem);
+            // Im Mini-plan den Ausschnit hervorheben
+            console.log($elem);
+            $(elem).addClass('ausschnitt-aktuell');
 
+            $('.sitz').off();
+
+            let scope = this;
+            // neue click events:
+            $('.sitz').click(function () {
+                scope.waehleSitz(this);
+            });
 
         }
-
-
 
 
     }
 
-    waehleSitz(elem: any | HTMLElement) {
+    /**
+     *
+     *
+     * @param {any | HTMLElement} elem
+     */
+    waehleSitz = (elem: any | HTMLElement) => {
 
         console.log(elem);
         elem.classList.toggle('ausgewaehlt');
 
-    }
+    };
 
+    /**
+     *
+     *
+     * @param elem
+     */
     hoverInfo(elem: any) {
 
-
-        const elem_hover_info = document.getElementById('sitz-hover-info');
 
         // Die ID des Sitzelements
         const elem_id = elem.getAttribute('id');
 
-        // die Position des sitz-elements speichern
-        const offsets = elem.getBoundingClientRect();
-        const top = offsets.top;
-        const left = offsets.left;
-
-        // den Hover an dieser Position anzeigen
-        $(elem_hover_info).show();
-        $(elem_hover_info).css('top', top,).css('left', left,);
-
 
         // Daten extrahieren:
-        const abschnitt = elem.parentNode.parentNode.dataset.abschnitt;
-        const seite = elem.parentNode.parentNode.dataset.seite;
-        const reihe_nummer = elem.parentNode.dataset.reihe;
-        //  const sitz_nummer = elem_id.slice(3);
-        const sitz_preis = elem.dataset.preis;
+        let abschnitt: string = elem.parentNode.parentNode.dataset.abschnitt;
+        let seite: string = elem.parentNode.parentNode.dataset.seite;
+        let reihe_nummer: string = elem.parentNode.dataset.reihe;
+        let sitz_nummer: string = '99';
+        let preis: string = elem.dataset.preis;
 
 
-        // das HTML an den
-        console.log(elem);
+        if (elem_id) {
+            sitz_nummer = elem_id.replace('sitz-', '');
+        }
+
+
+        // felder füllen:
+        $('.container-hover-info-abschnitt').html(abschnitt);
+        $('.container-hover-info-seite').html(seite);
+        $('.container-hover-info-reihe-nummer').html(reihe_nummer);
+        $('.container-hover-info-sitz-nummer').html(sitz_nummer);
+        $('.container-hover-info-preis').html(preis);
+
+        // nicht im uebersichts-modus
+
+        if (this.modus_maxiplan) {
+
+            // Abschnitt-Ansicht
+
+
+            // als Hover anzeigen
+            const abschnitt_hover_info = document.getElementById('abschnitt-hover-info');
+
+            // die Position des sitz-elements speichern
+            const offsets = elem.getBoundingClientRect();
+            const top = offsets.top;
+            const left = offsets.left;
+
+            // den Hover an dieser Position anzeigen
+            $(abschnitt_hover_info).show();
+            $(abschnitt_hover_info).css('top', top,).css('left', left,);
+
+
+            const $abschnitt = elem.parentElement;
+
+            console.log($abschnitt);
+
+        } else {
+
+            // Einzel-Sitz-Ansicht
+
+            // als Hover anzeigen
+            const sitz_hover_info = document.getElementById('sitz-hover-info');
+
+            // die Position des sitz-elements speichern
+            const offsets = elem.getBoundingClientRect();
+            const top = offsets.top;
+            const left = offsets.left;
+
+            // den Hover an dieser Position anzeigen
+            $(sitz_hover_info).show();
+            $(sitz_hover_info).css('top', top,).css('left', left,);
+
+        }
 
 
     }
--- web/js/controllers/SeatSelectController.js
+++ web/js/controllers/SeatSelectController.js
@@ -1,83 +1,184 @@
 var SeatSelectController = (function () {
     function SeatSelectController() {
+        var _this = this;
+        this.modus_maxiplan = false;
+        this.modus_miniplan = false;
+        this.updateModus = function () {
+            var scope = _this;
+            if ($(window).width() > 500) {
+                // Desktop Screen
+                _this.modus_maxiplan = false;
+                _this.modus_miniplan = false;
+                $('#plan-well').removeClass('modus-maxiplan');
+            }
+            else {
+                // do something for small screens
+                _this.modus_maxiplan = true;
+                _this.modus_miniplan = false;
+                $('#plan-well').addClass('modus-maxiplan');
+                $('.abschnitt').addClass('modus-maxiplan-abschnitt-target');
+                $('.modus-maxiplan-abschnitt-target').click(function () {
+                    scope.showDetails(this);
+                });
+            }
+            console.log('modus_maxiplan: ' + _this.modus_maxiplan);
+            console.log('modus_miniplan: ' + _this.modus_miniplan);
+        };
+        /**
+         *
+         *
+         *
+         */
+        this.addEventListeners = function () {
+            // toggle
+            var scope = _this;
+            $('.sitz').click(function () {
+                scope.waehleSitz(this);
+            });
+            $('table.preiskategorien tr').mouseover(_this, function () {
+                scope.abschnittHervorheben(this);
+            });
+            $('.reihe div.sitz').mouseover(_this, function () {
+                scope.hoverInfo(this);
+            });
+            $('#plan-content').mouseleave(function () {
+                $('#sitz-hover-info').hide();
+            });
+        };
+        /**
+         *
+         *
+         * @param {any | HTMLElement} elem
+         */
+        this.waehleSitz = function (elem) {
+            console.log(elem);
+            elem.classList.toggle('ausgewaehlt');
+        };
         console.log('seatSelectController');
     }
     SeatSelectController.prototype.start = function () {
-        this.addEventListeners();
         sitzeVierschieben('links');
         sitzeVierschieben('rechts');
-    };
-    SeatSelectController.prototype.addEventListeners = function () {
-        // toggle
+        // Modus erkennen
+        // init
+        this.updateModus();
+        this.addEventListeners();
+        // after change
+        var rtime;
+        var timeout = false;
+        var delta = 200;
         var scope = this;
-        $('.abschnitt').click(function () {
-            if ($(window).width() < 500) {
-                // do something for small screens
-                scope.showDetails(this);
+        $(window).resize(function () {
+            rtime = new Date();
+            if (timeout === false) {
+                timeout = true;
+                setTimeout(resizeend, delta);
             }
         });
-        $('.sitz').click(function () {
-            if ($(window).width() > 500) {
-                // do something for small screens
-                scope.waehleSitz(this);
+        function resizeend() {
+            var next_date = new Date();
+            if (next_date - rtime < delta) {
+                setTimeout(resizeend, delta);
             }
             else {
-                scope.showDetails(this);
+                timeout = false;
+                scope.updateModus();
             }
-            // Wenn nur Abschnitt zu sehen ist
-        });
-        $('table.preiskategorien tr').mouseover(this, function () {
-            scope.abschnittHervorheben(this);
-        });
-        $('.reihe div.sitz').mouseover(this, function () {
-            scope.hoverInfo(this);
-        });
-        $('#plan-content').mouseleave(function () {
-            $('#sitz-hover-info').hide();
-        });
+        }
     };
+    /**
+     *
+     * @param elem
+     */
     SeatSelectController.prototype.showDetails = function (elem) {
-        var $plan = $('#plan-content');
-        var $abschnitt_container = $('#abschnitt-container');
+        var scope = this;
+        // Anleitung ausblenden
+        $('#sitzplan-content .block-anleitung').hide();
+        // aufräumen
+        $('.ausschnitt-aktuell').removeClass('ausschnitt-aktuell');
+        var $well = $('#plan-well');
+        var $abschnitt_container = $('#plan-ausschnitt-container');
         var $elem = $(elem);
-        var class_uebersicht = 'uebersicht-modus';
+        var modus_miniplan = 'modus-miniplan';
         // übersichts Modus?
-        if ($plan.hasClass(class_uebersicht)) {
-            $plan.removeClass(class_uebersicht);
+        if (this.modus_miniplan) {
+            $well.removeClass(modus_miniplan);
             $abschnitt_container.empty();
+            this.modus_miniplan = false;
         }
         else {
+            // Setze das System in den MIniplan:
+            this.modus_miniplan = true;
+            // Der Abschnitt darf
             $abschnitt_container.empty();
-            $plan.addClass(class_uebersicht);
+            $well.addClass(modus_miniplan);
             var $clone = $elem.clone(true);
+            $clone.find('.sitz').addClass('modus_miniplan');
+            // Den Click-Event entfernen
+            $clone.first().off();
+            $clone.removeClass('modus-maxiplan-abschnitt-target');
             $clone.appendTo($abschnitt_container);
-            $plan.addClass(class_uebersicht);
-            console.log(elem);
+            // Im Mini-plan den Ausschnit hervorheben
+            console.log($elem);
+            $(elem).addClass('ausschnitt-aktuell');
+            $('.sitz').off();
+            var scope_1 = this;
+            // neue click events:
+            $('.sitz').click(function () {
+                scope_1.waehleSitz(this);
+            });
         }
     };
-    SeatSelectController.prototype.waehleSitz = function (elem) {
-        console.log(elem);
-        elem.classList.toggle('ausgewaehlt');
-    };
+    /**
+     *
+     *
+     * @param elem
+     */
     SeatSelectController.prototype.hoverInfo = function (elem) {
-        var elem_hover_info = document.getElementById('sitz-hover-info');
         // Die ID des Sitzelements
         var elem_id = elem.getAttribute('id');
-        // die Position des sitz-elements speichern
-        var offsets = elem.getBoundingClientRect();
-        var top = offsets.top;
-        var left = offsets.left;
-        // den Hover an dieser Position anzeigen
-        $(elem_hover_info).show();
-        $(elem_hover_info).css('top', top).css('left', left);
         // Daten extrahieren:
         var abschnitt = elem.parentNode.parentNode.dataset.abschnitt;
         var seite = elem.parentNode.parentNode.dataset.seite;
         var reihe_nummer = elem.parentNode.dataset.reihe;
-        //  const sitz_nummer = elem_id.slice(3);
-        var sitz_preis = elem.dataset.preis;
-        // das HTML an den
-        console.log(elem);
+        var sitz_nummer = '99';
+        var preis = elem.dataset.preis;
+        if (elem_id) {
+            sitz_nummer = elem_id.replace('sitz-', '');
+        }
+        // felder füllen:
+        $('.container-hover-info-abschnitt').html(abschnitt);
+        $('.container-hover-info-seite').html(seite);
+        $('.container-hover-info-reihe-nummer').html(reihe_nummer);
+        $('.container-hover-info-sitz-nummer').html(sitz_nummer);
+        $('.container-hover-info-preis').html(preis);
+        // nicht im uebersichts-modus
+        if (this.modus_maxiplan) {
+            // Abschnitt-Ansicht
+            // als Hover anzeigen
+            var abschnitt_hover_info = document.getElementById('abschnitt-hover-info');
+            // die Position des sitz-elements speichern
+            var offsets = elem.getBoundingClientRect();
+            var top_1 = offsets.top;
+            var left = offsets.left;
+            // den Hover an dieser Position anzeigen
+            $(abschnitt_hover_info).show();
+            $(abschnitt_hover_info).css('top', top_1).css('left', left);
+            var $abschnitt = elem.parentElement;
+            console.log($abschnitt);
+        }
+        else {
+            // Einzel-Sitz-Ansicht
+            // als Hover anzeigen
+            var sitz_hover_info = document.getElementById('sitz-hover-info');
+            // die Position des sitz-elements speichern
+            var offsets = elem.getBoundingClientRect();
+            var top_2 = offsets.top;
+            var left = offsets.left;
+            // den Hover an dieser Position anzeigen
+            $(sitz_hover_info).show();
+            $(sitz_hover_info).css('top', top_2).css('left', left);
+        }
     };
     SeatSelectController.prototype.abschnittHervorheben = function (elem) {
         // Aus Performancegründen kein JQuery nehmen !
